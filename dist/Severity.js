d6bf6bb2d2a46fb9f59366d28e1c1cfce668dc43
Radek Balko <radek.balko@quest.com>
2019-02-27 09:06:44 +0100

    sandboxing

--- dist/Severity.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var Severity;
-(function (Severity) {
-    Severity["Info"] = "INFO";
-    Severity["Error"] = "ERROR";
-    Severity["Warning"] = "WARNING";
-})(Severity || (Severity = {}));
-//# sourceMappingURL=Severity.js.map
